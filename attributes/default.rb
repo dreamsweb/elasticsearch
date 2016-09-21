d52ade2696415a9ef084aee2be6fcfca46c7007c
Nick C <nick.chernyshev@gmail.com>
2016-09-21 11:32:01 +0200

    update elasticsearch

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default.elasticsearch[:version]  = "1.1.0"
-default.elasticsearch[:deb_url]  = "https://download.elasticsearch.org/elasticsearch/elasticsearch/elasticsearch-#{node.elasticsearch[:version]}.deb"
-default.elasticsearch[:deb_sha1] = "2cecb08a388948c3c6300ca56e0aad11796c3109"
+default.elasticsearch[:version]  = "2.4.0"
+default.elasticsearch[:deb_url]  = "https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/#{node.elasticsearch[:version]}/elasticsearch-#{node.elasticsearch[:version]}.deb"
+default.elasticsearch[:deb_sha1] = "2a6c79c936ce6fb66cbde39a2e8e344e8b361dcc"
