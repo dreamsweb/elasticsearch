0147692691d71d67e5462280451d4ada4e9ca422
Nick C <nick.chernyshev@gmail.com>
2017-06-30 15:36:51 +0200

    fix attributes

--- attributes/default.rb
+++ attributes/default.rb
@@ -1,3 +1,3 @@
-default.elasticsearch[:version]  = "2.4.0"
-default.elasticsearch[:deb_url]  = "https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/#{node.elasticsearch[:version]}/elasticsearch-#{node.elasticsearch[:version]}.deb"
-default.elasticsearch[:deb_sha1] = "2a6c79c936ce6fb66cbde39a2e8e344e8b361dcc"
+default['elasticsearch']['version']  = "2.4.0"
+default['elasticsearch']['deb_url']  = "https://download.elastic.co/elasticsearch/release/org/elasticsearch/distribution/deb/elasticsearch/#{node['elasticsearch']['version']}/elasticsearch-#{node['elasticsearch']['version']}.deb"
+default['elasticsearch']['deb_sha1'] = "2a6c79c936ce6fb66cbde39a2e8e344e8b361dcc"
--- metadata.rb
+++ metadata.rb
@@ -3,7 +3,7 @@ maintainer        "Dwwd"
 maintainer_email  "info@dwwd.ru"
 description       "Basic elasticsearch install"
 long_description IO.read(File.join(File.dirname(__FILE__), 'README.md'))
-version           "0.0.2"
+version           "0.0.3"
 
 recipe "elasticsearch", "Installs ElasticSearch"
 
