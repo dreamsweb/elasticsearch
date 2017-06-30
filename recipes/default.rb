02fda5c0a64064c87d513b165ef591d56840a1f8
Nick C <nick.chernyshev@gmail.com>
2017-06-30 15:39:42 +0200

    fix recipe

--- recipes/default.rb
+++ recipes/default.rb
@@ -7,11 +7,11 @@
 
 include_recipe "java"
 
-filename = node.elasticsearch[:deb_url].split('/').last
+filename = node['elasticsearch']['deb_url'].split('/').last
 
 remote_file "#{Chef::Config[:file_cache_path]}/#{filename}" do
-  source   node.elasticsearch[:deb_url]
-  checksum node.elasticsearch[:deb_sha]
+  source   node['elasticsearch']['deb_url']
+  checksum node['elasticsearch']['deb_sha']
   mode 00644
 end
 
@@ -20,7 +20,7 @@ dpkg_package "#{Chef::Config[:file_cache_path]}/#{filename}" do
 end
 
 service "elasticsearch" do
-  supports :restart => true, :status => true
+  supports restart: true, status: true
 
   action [:enable, :start]
 end
